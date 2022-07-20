a78b9302337616c26ccce2d32daa5666140f23d0
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2022-07-20 21:48:04 +0200

    change folders

--- scripts/install_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo pip3 install -y virtualenv poetry
-cd /home/ec2-user/app
-virtualenv whale-api-venv
-source whale-api-venv/bin/activate
-sudo poetry install
--- scripts/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/app/
-source whale-api-venv/bin/activate
-python3 src/main.py
