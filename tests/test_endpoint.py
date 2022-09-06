838cbde337ec93ed3fe979b51ded2e4c11c3eac0
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2022-09-06 22:24:13 +0200

    update test

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -3,4 +3,6 @@ from starlette.testclient import TestClient
 
 def test_health(client: TestClient):
     response = client.get("/")
-    assert response.json() == {"healthy": True}
+    assert response.status_code == 200
+    assert response.text == "OK"
+
