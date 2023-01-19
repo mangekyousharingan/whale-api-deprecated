76aafec39acffcf05727f39e65626ea4965b63f3
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-01-19 11:41:24 +0100

    add two adapters

---
+++ src/adapters/coin_market_cap.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CoinMarketCap:
+    api_key: str
+
---
+++ src/adapters/taapi.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TaApi:
+    pass
+
+#  TODO: taAPI integration wih technical analysis indicators
