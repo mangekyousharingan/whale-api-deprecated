467897180364afafe42ace08187949775275c7b2
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-06-12 20:01:27 +0200

    Reorgenaize project

--- src/app.py
+++ src/app.py
@@ -1,57 +1,19 @@
-from dataclasses import dataclass, field
-from typing import Optional
-
-import uvicorn
 from fastapi import FastAPI
 
-from adapters.mempool import MempoolSpaceAdapter
-from controllers.http import make_http_controller
-from ports.DataPort import DataPort
-from core.usecases.address_txs_balance_sheet import AddressTxBalance
-from core.usecases.address_wealth import AddressWealth
 
-
-@dataclass
 class WhaleAPI:
-    http_controller: Optional[FastAPI] = None
-    config: dict = field(default_factory=dict)
-    adapters: dict = field(default_factory=dict)
-    env: Optional[str] = None
+    http_controller: FastAPI
 
-    def load_config(self) -> None:
-        print("Loading config...")
+    def _set_up(self):
+        pass
 
-    def load_adapters(self) -> None:
-        self.adapters = {"btc": MempoolSpaceAdapter()}
+    def _create_controller(self):
+        pass
 
-    def _adapters_factory(self, coin: str) -> DataPort:
-        try:
-            adapter = self.adapters[coin]
-        except KeyError as error:
-            raise Exception(f"Adapter for {coin} is not supported!")
-        else:
-            return adapter
+    def _start_service(self):
+        pass
 
-    def set_up(self) -> None:
-        address_wealth_usecase = AddressWealth(self._adapters_factory)
-        address_tz_balance_usecase = AddressTxBalance(self._adapters_factory)
-        self.http_controller = make_http_controller(
-            address_wealth_usecase, address_tz_balance_usecase
-        )
-
-    def start_service(self) -> None:
-        print("Starting service...")
-        uvicorn.run(self.http_controller, port=8080, host="0.0.0.0")
-
-    def set_up_and_start_service(self) -> None:
-        self.load_config()
-        self.load_adapters()
-        self.set_up()
-        self.start_service()
-
-
-#  TODO: fix mypy issues
-#  TODO: implement other use cases
-#  TODO: auth
-#  TODO: adapters factory
-#  TODO: exception handling
+    def set_up_and_start_service(self):
+        self._set_up()
+        self._create_controller()
+        self._start_service()
--- tests/test_endpoint.py
+++
@@ -1,7 +0,0 @@
-from starlette.testclient import TestClient
-
-
-def test_health(client: TestClient):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.text == "OK"
